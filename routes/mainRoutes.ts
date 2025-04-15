1eb42c33193b140b543037303d8e01dfc97c6098
Ysa-04 <ysaura2903@outlook.com>
2025-04-15 16:58:25 +0000

    snoeien en vellen

--- routes/mainRoutes.ts
+++ routes/mainRoutes.ts
@@ -8,6 +8,9 @@ router.get("/", (req, res) => {
 router.get("/contact", (req, res) => {
     res.render('contact');
 });
+router.get("/snoeien-en-vellen", (req,res) => {
+  res.render('snoeien-en-vellen');
+})
 
 
 // meerdere routers invoegen hieronder zoals router.get hierboven
